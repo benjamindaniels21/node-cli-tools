0812280f022ed90eab948691c5b1ff14a0b4c47f
Ben Daniels <benjamindaniels21@gmail.com>
2023-02-18 13:55:41 -0500

    updates

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ console.log(counterObj.getCounter());
 const newCounterObj = require("./myscript");
 
 console.log(newCounterObj.getCounter());
+
+newCounterObj.incrementCounter();
+
+console.log(newCounterObj.getCounter());
