31800b85b87a15f9da4ec248b31b7ee8981fc907
Ben Daniels <benjamindaniels21@gmail.com>
2023-02-18 15:58:56 -0500

    list files in current directory

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const fs = require("fs"); // require the file system module in node
 const path = require("path"); // require path module
 
-fs.readdir(".", (err) => {
+fs.readdir(".", (err, filenames) => {
   //either err === an error obj, which means there was an error
   //or err is null which means it's ok
 
   if (err) {
     //error handling code
-    throw new Error();
+    console.log(err);
+    return;
   } else {
-    console.log("FINNNNE");
+    console.log(filenames);
   }
 });
